23c7e888a6c29c3734b262674795db6a25aa144d
Omar Tolba <omartolba004@gmail.com>
2022-12-24 15:43:32 +0200

    added layered arch

--- MCAL/IntCtrl/Inc/IntCtrl_Types.h
+++ MCAL/IntCtrl/Inc/IntCtrl_Types.h
@@ -17,21 +17,24 @@
 /**********************************************************************************************************************
  * CONSTANT MACROS
  *********************************************************************************************************************/
-#define SCB_TOTAL_NUMBER_OF_EXCEPTIONS      10
-#define INTCTRL_TOTAL_NUMBER_OF_INTERRUPTS      1
+#define SCB_TOTAL_NUMBER_OF_EXCEPTIONS 10
+#define INTCTRL_TOTAL_NUMBER_OF_INTERRUPTS 1
 
 /**********************************************************************************************************************
  * INCLUDES
  *********************************************************************************************************************/
 #include "Std_Types.h"
 
+/**********************************************************************************************************************
+ *  GLOBAL DATA PROTOTYPES
+ *********************************************************************************************************************/
 typedef enum
 {
-EIGHT_GROUPS_ONE_SUBGROUP = 0x04,
-FOUR_GROUPS_TWO_SUBGROUP = 0x05,
-TWO_GROUPS_FOUR_SUBGROUP = 0x06,
-ONE_GROUPS_EIGHT_SUBGROUP = 0x07,
-}IntCtrl_GROUPING_TYPES;
+    EIGHT_GROUPS_ONE_SUBGROUP = 0x04,
+    FOUR_GROUPS_TWO_SUBGROUP = 0x05,
+    TWO_GROUPS_FOUR_SUBGROUP = 0x06,
+    ONE_GROUPS_EIGHT_SUBGROUP = 0x07,
+} IntCtrl_GROUPING_TYPES;
 
 typedef enum
 {
@@ -45,7 +48,7 @@ typedef enum
     DEBUG_MONITOR = 12,
     PENDSV = 14,
     SYSTICK = 15,
-}SCB_EXC_TYPES;
+} SCB_EXC_TYPES;
 
 typedef uint8 IntCtrl_InerruptTypes;
 
@@ -58,23 +61,17 @@ typedef uint8 IntCtrl_GroupPriorityType;
 /* To hold the value of sub group priority*/
 typedef uint8 IntCtrl_SubGroupPriorityType;
 
-typedef struct 
+typedef struct
 {
-IntCtrl_PeripheralGateType peripheralGateType;
-IntCtrl_GroupPriorityType groupPriorityType;
-IntCtrl_SubGroupPriorityType subGroupPriorityType;
-}IntCtrl_ConfigType;
+    IntCtrl_PeripheralGateType peripheralGateType;
+    IntCtrl_GroupPriorityType groupPriorityType;
+    IntCtrl_SubGroupPriorityType subGroupPriorityType;
+} IntCtrl_ConfigType;
 
-
-typedef struct 
+typedef struct
 {
-IntCtrl_ConfigType configType[SCB_TOTAL_NUMBER_OF_EXCEPTIONS+INTCTRL_TOTAL_NUMBER_OF_INTERRUPTS];
-}IntCtrl_ConfigTypeArr;
-
-
-/**********************************************************************************************************************
- *  GLOBAL DATA PROTOTYPES
- *********************************************************************************************************************/
+    IntCtrl_ConfigType configType[SCB_TOTAL_NUMBER_OF_EXCEPTIONS + INTCTRL_TOTAL_NUMBER_OF_INTERRUPTS];
+} IntCtrl_ConfigTypeArr;
 
 #endif /* INTCTRL_TYPES_H */
 /**********************************************************************************************************************
